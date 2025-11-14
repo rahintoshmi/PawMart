f739930e61c7972ccaf03b7178e79d7b11938cb6
rahintoshmi <2204ohee@gmail.com>
2025-11-14 16:36:42 +0600

    error fix

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,8 @@ import { Toaster } from 'react-hot-toast';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}  />
       <Toaster position="top-center" reverseOrder={false} />
+      <RouterProvider router={router}  />
     </AuthProvider>
   </StrictMode>,
 )
